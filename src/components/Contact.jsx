d8df4a913583c326a3a90e813369c918e46408d3
nitesh agnihotri <niteshagni1830@gmail.com>
2022-01-21 02:21:48 +0530

    added proxy

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
         e.preventDefault();
         const { name, email, phone, message } = user;
 
-        const data = await fetch('/submit', {
+        const data = await fetch('https://appliance-plus.herokuapp.com/submit', {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
