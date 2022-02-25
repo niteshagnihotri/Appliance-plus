c453354a49b00bbda0d0a9cf239344bdd996c8d4
nitesh agnihotri <niteshagni1830@gmail.com>
2022-02-25 19:00:00 +0530

    udate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,5 @@
 import Main from './Main';
-// import About from './About';
 import Services from './Services';
-// import Work from './Work';
-// import Solution from './Solution';
 import Qoute from './Qoute';
 import Contact from './Contact';
 
@@ -10,12 +7,8 @@ function Home(){
     return (
         <>
             <Main/>
-            {/* <About/> */}
             <Services/>
-            {/* <Work/> */}
             <Qoute/>
-            {/* <Solution/> */}
-            {/* <Companies/> */}
             <Contact/>
         </>
 
